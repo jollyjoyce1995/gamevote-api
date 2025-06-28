18d179394278876b695302ce6d95f29709a69141
lucky <wiiinator1@gmail.com>
2025-06-28 09:51:21 +0200

    * added column to beer entity

--- src/main/kotlin/at/tailor/gamevoteapi/party/service/persistence/BeerEntity.kt
+++ src/main/kotlin/at/tailor/gamevoteapi/party/service/persistence/BeerEntity.kt
@@ -5,10 +5,16 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.ManyToOne
+import java.time.LocalDateTime
 
 @Entity
 data class BeerEntity(
     @Id @GeneratedValue var id: Long = 0,
     @ManyToOne var party: PartyEntity? = null,
-    @Column var attendee: String = ""
-)
+    @Column var attendee: String = "",
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL") var createdAt: LocalDateTime = LocalDateTime.now(),
+) {
+    companion object {
+        const val TABLE_NAME = "beer_entity"
+    }
+}
